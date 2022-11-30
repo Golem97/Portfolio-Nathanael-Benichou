f50c4913a7189a53e13e13332f0190721247f793
Golem97 <nathbenich736@gmail.com>
2022-11-30 16:15:01 +0100

    first

---
+++ gitprofile.config.js
@@ -0,0 +1,147 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'Golem97', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 10, // How many projects to display.
+    exclude: {
+      forks: false, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: 'nathanael-benichou-29b815117',
+    twitter: '',
+    facebook: '',
+    instagram: '',
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '14884221/naben', // format: userid/username
+    website: 'https://nathanael-benichou.fr',
+    phone: '',
+    email: 'nathbenich736@gmail.com',
+  },
+  resume: {
+    fileUrl: 'https://drive.google.com/file/d/10krRl0HK5K1gqtztEpbvuKLdzaszyN-3/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'JavaScript',
+    'Java',
+    'C++',
+    'Bash',
+    'React.js',
+    'Node.js',
+    'Express.js',
+    'Next.js',
+    'MongoDB',
+    'Material-UI',
+    'Git',
+    'HTML5',
+    'Boostrap',
+    'CSS',
+    'Canva',
+  ],
+  experiences: [
+    {
+      company: '2B Digital',
+      position: 'Full-Stack Developper',
+      from: 'Februar 22',
+      to: 'Present',
+      companyLink: 'https://2bdigital.co',
+    },
+    {
+      company: 'Just A Site',
+      position: 'FrontEnd Developper',
+      from: '2018',
+      to: 'Februar 22',
+      companyLink: '',
+    },
+  ],
+  education: [
+    {
+      institution: 'Ariel University',
+      degree: 'Computer Science',
+      from: '2019',
+      to: '2023',
+    },
+   
+  ],
+  // Display blog posts from your medium or dev account. (Optional)
+  // blog: {
+  //   source: '', // medium | dev
+  //   username: '', // to hide blog section, keep it empty
+  //   limit: , // How many posts to display. Max is 10.
+  // },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'halloween',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+};
+
+export default config;
